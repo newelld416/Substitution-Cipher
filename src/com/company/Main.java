66fa2dd995d21e49f39588d09132b5d868d8e7c6
Daniel Newell <newell_daniel@yahoo.com>
2014-09-02 13:03:01 -0400

    Third Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,37 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static final String DECRYPT = "decrypt";
+    private static final String ENCRYPT = "encrypt";
+    private static final String FILE_PATH = "C:\\School Workspaces\\Substitution-Cipher\\src\\com\\company\\EncryptionKey.txt";
+
+    /**
+     * This is the main method of the program.
+     * @param args
+     */
     public static void main(String[] args) {
-        TranslateEngine translateEngine = new TranslateEngine(GetEncryptionKey());
+        TranslateEngine translateEngine = new TranslateEngine(FILE_PATH);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter decrypt or encrypt: ");
+        String cipher = scanner.nextLine();
+        if (cipher.toLowerCase().equals(DECRYPT)) {
+            System.out.println("You chose to decrypt a message");
+            System.out.print("Please enter the string you would like to decrypt: ");
+            String message = scanner.nextLine();
+            System.out.println("Your decrypted string is " + translateEngine.DecryptString(message));
+        } else if (cipher.toLowerCase().equals(ENCRYPT)) {
+            System.out.println("You chose to encrypt a message");
+            System.out.print("Please enter the string you would like to encrypt: ");
+            String message = scanner.nextLine();
+            System.out.println("Your decrypted string is " + translateEngine.EncryptString(message));
+        } else {
+            System.out.println("Sorry I do not understand that command, I give up on you.");
+        }
 
-        System.out.println("Encrypted String: " + args[0]);
-        System.out.println("Decrypted String: " + translateEngine.TranslateString(args[0]));
     }
 
-    private static String GetEncryptionKey() {
-        return "";
-    }
+
 }
--- src/com/company/TranslateEngine.java
+++ src/com/company/TranslateEngine.java
@@ -1,22 +1,130 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 /**
  * Created by Daniel Newell on 9/1/2014.
  */
 public class TranslateEngine {
 
-    private String EncryptionKey = "";
-    public TranslateEngine(String encryptionKey) {
-        EncryptionKey = encryptionKey;
+    private char[][] cipher = new char[54][2];
+    private String standardString = "";
+    private  String codedString = "";
+    private String[] bothStrings = new String[2];
+
+
+    /**
+     * This is the constructor method for this class.
+     * @param filePath
+     */
+    public TranslateEngine(String filePath) {
+        ReadCipherFile(filePath);
     }
 
-    public String TranslateString(String encryptedString) {
-        return "";
+    /**
+     * This is a public method used to encrypt a plain string.
+     * @param decryptedString
+     * @return
+     */
+    public String EncryptString(String decryptedString) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < decryptedString.length(); i++){
+            sb.append(EncryptCharacter(decryptedString.charAt(i)));
+        }
+
+        return sb.toString();
     }
 
-    private char TranslateCharacter(char encryptedCharacter){
-        return 'a';
+    /**
+     * This is a private method used to encrypt a single plain character.
+     * @param decryptedCharacter
+     * @return
+     */
+    private char EncryptCharacter(char decryptedCharacter){
+        if (decryptedCharacter != ' ') {
+            for (int i = 0; i < 54; i++) {
+                if (cipher[i][0] == decryptedCharacter) {
+                    return cipher[i][1];
+                }
+            }
+        }
+
+        return decryptedCharacter;
     }
 
+    /**
+     * This is a public method used to Decrypt an encrypted string.
+     * @param encryptedString
+     * @return
+     */
+    public String DecryptString(String encryptedString) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < encryptedString.length(); i++){
+            sb.append(DecryptCharacter(encryptedString.charAt(i)));
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * This is a private method used to decrypt a single encrypted character.
+     * @param encryptedCharacter
+     * @return
+     */
+    private char DecryptCharacter(char encryptedCharacter) {
+        if (encryptedCharacter != ' ') {
+            for (int i = 0; i < 54; i++) {
+                if (cipher[i][1] == encryptedCharacter) {
+                    return cipher[i][0];
+                }
+            }
+        }
+        return encryptedCharacter;
+    }
+
+    /**
+     * This is a private method that reads the cipher file designated in the constructor.
+     * @param fileName
+     */
+    private void ReadCipherFile(String fileName){
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+
+            PopulateCipherArray(sb.toString().split(System.lineSeparator()));
+
+            br.close();
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.toString());
+        }
+    }
+
+    /**
+     * This is a private method used to populate the cipherArray used in encryption and decryption.
+     * @param bothStrings
+     */
+    private void PopulateCipherArray(String[] bothStrings){
+        if (bothStrings.length == 2) {
+            standardString = bothStrings[0];
+            for (int i = 0; i < standardString.length(); i++) {
+                cipher[i][0] = standardString.charAt(i);
+            }
+
+            codedString = bothStrings[1];
+            for (int i = 0; i < codedString.length(); i++) {
+                cipher[i][1] = codedString.charAt(i);
+            }
+        }
+    }
 
 }

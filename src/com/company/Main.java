650190bcb572ad38ccddc6aa3f8cd1575d5bda0a
Daniel Newell <newell_daniel@yahoo.com>
2014-09-01 22:54:55 -0400

    Second Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        TranslateEngine translateEngine = new TranslateEngine(GetEncryptionKey());
+
+        System.out.println("Encrypted String: " + args[0]);
+        System.out.println("Decrypted String: " + translateEngine.TranslateString(args[0]));
+    }
+
+    private static String GetEncryptionKey() {
+        return "";
     }
 }
---
+++ src/com/company/TranslateEngine.java
@@ -0,0 +1,22 @@
+package com.company;
+
+/**
+ * Created by Daniel Newell on 9/1/2014.
+ */
+public class TranslateEngine {
+
+    private String EncryptionKey = "";
+    public TranslateEngine(String encryptionKey) {
+        EncryptionKey = encryptionKey;
+    }
+
+    public String TranslateString(String encryptedString) {
+        return "";
+    }
+
+    private char TranslateCharacter(char encryptedCharacter){
+        return 'a';
+    }
+
+
+}

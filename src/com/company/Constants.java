7550a13b68007bd76af97264cd2b057b3ba33a1b
Daniel Newell <newell_daniel@yahoo.com>
2014-09-09 21:25:26 -0400

    Extra Commit

--- src/com/company/Constants.java
+++ src/com/company/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
     public static final String DECRYPT = "decrypt";
     public static final String ENCRYPT = "encrypt";
     public static final String FILE_PATH = "C:\\School Workspaces\\Substitution-Cipher\\EncryptionKey.txt";
-    public static final boolean PRINT_TO_FILE = true;
+    public static final boolean PRINT_TO_FILE = false;
     public static final String PlainTextBackward = "ZYXWVUTSRQPONMLKJIHGFEDCBAzyxwvutsrqponmlkjihgfedcba";
     public static final String PlainTextForward = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     public static final String OUTPUT_FILENAME = "Output.txt";
@@ -23,4 +23,6 @@ public class Constants {
     public static final String OUTPUT_MESSAGE = "Your message has been written out to the file %s";
     public static final String KEYWORD_MESSAGE = "Please enter the keyword you wish to create the cipher with: ";
     public static final String SUMMARY_MESSAGE = "This program allows you to create a Caesar Cipher using any keyword that you choose. \n";
+    public static final String CONTINUE_MESSAGE = "Would you like to continue (Yes or No): ";
+    public static final String YES = "yes";
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,32 +15,39 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         boolean ValidInput = false;
         String choice = "";
-
-        System.out.println(Constants.SUMMARY_MESSAGE);
-        System.out.print(Constants.KEYWORD_MESSAGE);
-        String keyWord = scanner.nextLine();
-        TranslateEngine translateEngine = new TranslateEngine(keyWord);
+        String continueMessage = "";
 
         do {
-            System.out.print(Constants.START_MESSAGE);
-            String nextLine = scanner.nextLine();
-            if (nextLine.toLowerCase().equals(Constants.DECRYPT)) {
-                choice = TranslateEngine.EncryptionType.decrypt.toString();
-                ValidInput = true;
-            } else if (nextLine.toLowerCase().equals(Constants.ENCRYPT)) {
-                choice = TranslateEngine.EncryptionType.encrypt.toString();
-                ValidInput = true;
-            }
-        } while (!ValidInput);
+            System.out.println(Constants.SUMMARY_MESSAGE);
+            System.out.print(Constants.KEYWORD_MESSAGE);
+            String keyWord = scanner.nextLine();
+            TranslateEngine translateEngine = new TranslateEngine(keyWord);
 
-        if (!choice.isEmpty()) {
-            System.out.printf(Constants.CHOICE_MESSAGE, choice);
+            do {
+                System.out.print(Constants.START_MESSAGE);
+                String nextLine = scanner.nextLine();
+                if (nextLine.toLowerCase().equals(Constants.DECRYPT)) {
+                    choice = TranslateEngine.EncryptionType.decrypt.toString();
+                    ValidInput = true;
+                } else if (nextLine.toLowerCase().equals(Constants.ENCRYPT)) {
+                    choice = TranslateEngine.EncryptionType.encrypt.toString();
+                    ValidInput = true;
+                }
+            } while (!ValidInput);
+
+            if (!choice.isEmpty()) {
+                System.out.printf(Constants.CHOICE_MESSAGE, choice);
+                System.out.println();
+                System.out.printf(Constants.ENTER_STRING_MESSAGE, choice);
+                String message = scanner.nextLine();
+                TranslateEngine.EncryptionType encryptionType = choice.equals(Constants.DECRYPT) ? TranslateEngine.EncryptionType.decrypt : TranslateEngine.EncryptionType.encrypt;
+                translateEngine.OutputMessage(String.format(Constants.FINAL_MESSAGE, choice, translateEngine.EncryptDecryptString(message, encryptionType)), Constants.OUTPUT_FILENAME);
+            }
+
+            System.out.print(Constants.CONTINUE_MESSAGE);
+            continueMessage = scanner.nextLine().toLowerCase();
             System.out.println();
-            System.out.printf(Constants.ENTER_STRING_MESSAGE, choice);
-            String message = scanner.nextLine();
-            TranslateEngine.EncryptionType encryptionType = choice.equals(Constants.DECRYPT) ? TranslateEngine.EncryptionType.decrypt : TranslateEngine.EncryptionType.encrypt;
-            translateEngine.OutputMessage(String.format(Constants.FINAL_MESSAGE, choice, translateEngine.EncryptDecryptString(message,encryptionType)),Constants.OUTPUT_FILENAME);
-        }
+        } while (continueMessage.equals(Constants.YES));
     }
 
 
